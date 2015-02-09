0e8819f7a5ea0ab188860316fb80889d6928e0d9
Bryan Bauer <bauerbr@onid.oregonstate.edu>
2015-02-08 19:52:53 -0800

    added src file

---
+++ src/content1.php
@@ -0,0 +1,21 @@
+<?php
+//http://php.net/manual/en/function.session-start.php
+	session_start ();
+	if(session_status() == PHP_SESSION_ACTIVE){
+		if(empty($_POST['username'])) {
+			echo "A user-name must be entered<br>";
+			echo '<a href="../~bauerbr/login.php">Return to Login Page </a>';
+		}
+		$username1 = $_POST['username'];
+		
+		if(empty($_POST['username']) == FALSE) {
+			$_SESSION['username'] = $_POST['username'];
+			$_SESSION['visits'] = $_SESSION['visits'] + 1;
+			echo "Hello $username1 <br>";
+			echo "You have visited this website " . $_SESSION['visits'] . " Times<br>";
+			echo '<a href="../src/content2.php">Go to Page Content 2 </a><br>';
+		}
+}
+
+?>
+<a href="logout.php">Logout</a>
