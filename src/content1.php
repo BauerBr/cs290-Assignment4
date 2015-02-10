df345630148d85a5537966f26b53d017c279516d
Bryan Bauer <bauerbr@onid.oregonstate.edu>
2015-02-09 18:33:16 -0800

    Found a bug so fixed it!

--- src/content1.php
+++ src/content1.php
@@ -4,7 +4,8 @@
 	if(session_status() == PHP_SESSION_ACTIVE){
 		if(empty($_POST['username'])) {
 			echo "A user-name must be entered<br>";
-			echo '<a href="../~bauerbr/login.php">Return to Login Page </a>';
+			echo '<a href= "http://web.engr.oregonstate.edu/~bauerbr/src/login.php">Return to Login Page </a> <br>';
+			
 		}
 		$username1 = $_POST['username'];
 		
@@ -13,9 +14,9 @@
 			$_SESSION['visits'] = $_SESSION['visits'] + 1;
 			echo "Hello $username1 <br>";
 			echo "You have visited this website " . $_SESSION['visits'] . " Times<br>";
-			echo '<a href="../src/content2.php">Go to Page Content 2 </a><br>';
+			echo '<a href="http://web.engr.oregonstate.edu/~bauerbr/src/content2.php">Go to Page Content 2 </a><br>';
+			echo '<a href= "logout.php">Logout</a>';
 		}
 }
 
 ?>
-<a href="logout.php">Logout</a>
--- src/content2.php
+++ src/content2.php
@@ -3,15 +3,17 @@
 	session_start();
 	if(session_status() == PHP_SESSION_ACTIVE){
 		if(empty($_SESSION['username'])) {
-			echo '<a href="../bauerbr/src/content1.php">Go to Page Content 1 </a>';
+			echo "A user-name must be entered<br>";
+			echo 'http://web.engr.oregonstate.edu/~bauerbr/src/login.php"> Login Page </a>';
 		}	
 		if(empty($_SESSION['username']) == FALSE) {
 			$_SESSION['visits'] = $_SESSION['visits'] + 1;
 			echo "Hello ". $_SESSION['username'] . "<br>";
-			echo "You have visited this website " . $_SESSION['visits'] . " Times<br>";			
+			echo "You have visited this website " . $_SESSION['visits'] . " Times<br>";	
+			echo '<a href="logout.php">Logout</a>';	
 		}
 	}
 ?>
 
-<a href="logout.php">Logout</a>
+
  
